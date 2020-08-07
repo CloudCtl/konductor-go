9fdc0274e776c9c114da28cb04ee4711c46004b6
usrbinkat <usrbinkat@braincraft.io>
2020-08-07 14:28:35 +0000

    testing

--- cmd/init.go
+++ cmd/init.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+Copyright 2020 Kat Morgan <usrbinkat@braincraft.io>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -46,6 +46,9 @@ Konductor Init:
         fmt.Println(name)
         fmt.Println(configFile)
     },
+}
+
+func core() {
 }
 
 func init() {
