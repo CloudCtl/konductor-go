96944ac50523f9a2a07799eee950a743acd0533b
usrbinkat <usrbinkat@braincraft.io>
2020-08-07 03:42:08 +0000

    testing

--- cmd/init.go
+++ cmd/init.go
@@ -19,6 +19,11 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	dirPlatform  = (os.GetEnv("HOME"), + "deploy")
 )
 
 // initCmd represents the init command
@@ -32,7 +37,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
+		fmt.Println(dirPlatform)
 	},
 }
 
