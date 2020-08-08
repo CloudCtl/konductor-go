9037d6ef387bbfdcc9cb386298c4ce4ea691d657
usrbinkat <usrbinkat@braincraft.io>
2020-08-08 04:35:00 +0000

    testing

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -42,7 +42,7 @@ Konductor Engine Init:
   restricted & airgap environment capabilities.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		CoreInit()
+		CoreDeploy()
 	},
 }
 
@@ -50,7 +50,7 @@ func init() {
 	rootCmd.AddCommand(deployCmd)
 }
 
-func CoreInit() {
+func CoreDeploy() {
     CmdPluginRun()
 }
 
