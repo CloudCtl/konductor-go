7f74653d140146b45ecd1d1f5f70316158c062bc
usrbinkat <usrbinkat@braincraft.io>
2020-08-09 16:55:17 +0000

    testing

--- cmd/init.go
+++ cmd/init.go
@@ -55,11 +55,12 @@ func CoreRun() {
 }
 
 func CoreInfo() {
+    subnetids []string = (configyaml.Subnets.Private)
     var runvars string = "\n" +
       "  Openshift Version: " + configyaml.Openshift.Version  + "\n" +
+      "  AWS Secret:        " + configyaml.Auth.Secret        + "\n" +
       "  AWS Subnet CIDR:   " + configyaml.Cloud.CidrPrivate  + "\n" +
-      "  AWS Subnet IDs:    " + configyaml.Cloud.CidrPrivate  + "\n" +
-      "  AWS Secret:        " + configyaml.Auth.Secret        + "\n"
+      "  AWS Subnet IDs:    " + subnetids + "\n"
     fmt.Println(runvars)
     fmt.Println(configyaml.Subnets.Private)
 }
