d23560ea425233b58d2b8fbe39db6d2ed693f1fe
Chris Ruffalo <chris.ruffalo@gmail.com>
2020-10-14 20:27:53 -0400

    Refactored to  use shared sparta-libs code

--- build.sh
+++ build.sh
@@ -11,23 +11,7 @@ rm /bin/konductor 2>/dev/null
 rm -rf /root/konductor 2>/dev/null
 mkdir -p /tmp/bin
 
-plugins="
-    github.com/containercraft/konductor-go/cmd \
-"
-#   "github.com/containercraft/konductor-go/plugins/err" \
-#   "github.com/containercraft/konductor-go/plugins/log" \
-#   "github.com/containercraft/konductor-go/plugins/auth" \
-#   "github.com/mitchellh/mapstructure" \
-#   "golang.org/x/sys/unix" \
-#   "github.com/spf13/cobra" \
-#   "github.com/spf13/viper" \
-#   "github.com/go-git/go-git" \
-#   "github.com/go-git/go-git/plumbing" \
-
-for i in ${plugins}; do
-  ${goCmd} get -u ${i};
-done
-
+${goCmd} mod download
 ${goCmd} build
 
 cp -f ./dev /tmp/bin/konductor 2>/dev/null
